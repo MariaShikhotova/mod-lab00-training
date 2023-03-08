ce19272ac2f3c3d9486cea15170d952ca9cdda94
MariaShikhotova <126818543+MariaShikhotova@users.noreply.github.com>
2023-03-08 21:40:07 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,6 +1,9 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
-
 int64_t power(int64_t x, uint16_t n) {
-    return 0;
+  int64_t sum = 1;
+  for (int i = 0; i < n; i++) {
+    sum = sum * x;
+  }
+  return sum;
 }
